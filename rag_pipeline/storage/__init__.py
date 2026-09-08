1121ed884b05b73e23c944378d125aa806e9381d
Kr-Rishu <kumar05.rishu@gmail.com>
2026-09-08 15:23:21 +0530

    Update __init__.py

--- rag_pipeline/storage/__init__.py
+++ rag_pipeline/storage/__init__.py
@@ -1,6 +1,6 @@
 from .base import VectorStorageBackend
 from .turbovec_sqlite import TurbovecSqliteBackend
-from .s3_vectors import S3VectorsBackend
+# from .s3_vectors import S3VectorsBackend
 
 def get_storage_backend(backend_name, collection_name, **kwargs):
     if backend_name == 'turbovec_sqlite':
@@ -8,9 +8,9 @@ def get_storage_backend(backend_name, collection_name, **kwargs):
         force_rebuild = kwargs.get('force_rebuild', False)
         return TurbovecSqliteBackend(collection_dir=collection_dir, collection_name=collection_name, force_rebuild=force_rebuild)
     
-    elif backend_name == 's3_vectors':
-        kwargs.pop('collection_dir', None)
-        kwargs.pop('force_rebuild', None)
-        return S3VectorsBackend(collection_name=collection_name, **kwargs)
+    # elif backend_name == 's3_vectors':
+    #     kwargs.pop('collection_dir', None)
+    #     kwargs.pop('force_rebuild', None)
+    #     return S3VectorsBackend(collection_name=collection_name, **kwargs)
     
     raise ValueError(f'Unknown storage backend: {backend_name}')
